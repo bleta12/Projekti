fd281d8f7e00118a77142d676d6f2cd25b971b24
bleta12 <102874311+bleta12@users.noreply.github.com>
2023-12-08 08:22:51 +0100

    commit services

--- Paintings.php
+++ Paintings.php
@@ -39,7 +39,7 @@ crossorigin="anonymous"></script>
               
 
               </li>
-              <li class="item button"><a href="Sign.html">Log In</a></li>
+              <li class="item button"><a href="Sign.php">Log In</a></li>
               <li class="item button secondary"><a href="#">Sign Up</a></li>
               <li class="toggle"><span class="bars"></span></li>
           </ul>
