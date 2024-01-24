6fc9a9600edd9508fae111ef42b2963b0e0cfff2
riinesh <rs67349@ubt-uni.net>
2024-01-24 14:15:07 +0100

    .

--- Paintings.php
+++ Paintings.php
@@ -83,77 +83,13 @@
 
 
 
-
         </div>
+
+
+
+
+
     </body>
-
-</html>
-
-
-
-
-<body>
-    <footer class="footer">
-        <div class="kryesor">
-            <div class="rreshti">
-                <div class="kolon">
-                    <h4><i class='bx bxs-paint'></i>SA-SELL ART</h4>
-
-
-                    <p style="color: black;font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;font-size:20px;
-                    ">"What would life be if we had no courage to attempt anything"</p>
-
-
-
-
-                </div>
-
-
-                <div class="kolon">
-                    <h4>Address</h4>
-                    <ul>
-                        <li><a href="#"><i class='bx bxs-map-pin'></i>Lagjja Kalabria,10000 Prishtine,Kosove</a></li>
-                        <li><a href="#"><i class='bx bxs-home'></i>More About us</a></li>
-                    </ul>
-                </div>
-
-
-
-                <div class="kolon">
-                    <h4>KONTAKTE</h4>
-                    <ul>
-                        <li><a href="Services.html"><i class='bx bxs-info-circle'></i>info@sellart.net</a></li>
-                        <li><a href="#"><i class='bx bxs-phone'></i>+383 38 142 283</a></li>
-                    </ul>
-                </div>
-
-
-                <div class="kolon">
-                    <h4>Follow us on:</h4>
-                    <div class="linqet">
-
-
-                        <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-                        <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
-                        <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-                        <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
-
-
-
-                    </div>
-                </div>
-
-
-            </div>
-
-        </div>
-        <hr>
-
-        <p class="copyright">All rights reserved by &copy;Sell Art 2023</p>
-
-
-    </footer>
-
-</body>
+    <?php include "Footer.php" ?>
 
 </html>
