9d668c2340fe1f05b0b64a8021f1c181140c0131
riinesh <rs67349@ubt-uni.net>
2024-02-03 00:11:35 +0100

    ShoppingCart

--- ShopingCart.php
+++ ShopingCart.php
@@ -8,14 +8,32 @@
     <link rel="stylesheet" href="cssfiles/shop.css">
 
 </head>
+<style>
+    .buttoni {
+        margin-left: 20px;
+        padding: 8px 20px;
+        background-color: black;
+        color: white;
+        text-decoration: none;
+        border-radius: 4px;
+        font-size: 14px;
+        display: inline-block;
+
+    }
+
+    .buttoni:hover {
+        background-color: #0056b3;
+        text-decoration: none;
+    }
+</style>
 
 <?php include "Header.php" ?>
 
 <body>
     <div class="container my-5">
         <h1>SHOPPING CART</h1>
-        <h2>Lista e Porosive</h2>
-        <a class="btn btn-primary" href="Paintings.php" role="button">Kthehu ne shopping </a>
+        <h2>Order List</h2>
+        <a class="btn btn-primary" href="Paintings.php" role="button">Back to shopping </a>
         <br>
         <div class="table-responsive">
             <table class="table table-sm">
@@ -26,32 +44,31 @@
                         <th>Emri</th>
                         <th>Cmimi</th>
                         <th>Modifiko</th>
-                    </tr> 
+                    </tr>
                 </thead>
                 <tbody>
-                  <?php               
-                   include "OrderRepo.php";
-                  
-                      $user_id = isset($_SESSION['user']['ID']) ? $_SESSION['user']['ID'] : null;
-                      
+                    <?php
+                    include "OrderRepo.php";
 
-                      $orderRepo = new OrderRepo();
-                      $orders = $orderRepo->getOrderFromUser($user_id);
-                      $totaliCmimit=0;
-                      $totali=0;
-                      $transporti=0;
-                      if(!$orders){
-                        echo"No orders available";
-                      }else{
-                        foreach($orders as $order) {
-                            $totaliCmimit=$totaliCmimit+$order['Cmimi'];
-                            if($totaliCmimit===0){
-                                $transporti=0;
+                    $user_id = isset($_SESSION['user']['ID']) ? $_SESSION['user']['ID'] : null;
+
+
+                    $orderRepo = new OrderRepo();
+                    $orders = $orderRepo->getOrderFromUser($user_id);
+                    $totaliCmimit = 0;
+                    $totali = 0;
+                    $transporti = 0;
+                    if (!$orders) {
+                        echo "No orders available";
+                    } else {
+                        foreach ($orders as $order) {
+                            $totaliCmimit = $totaliCmimit + $order['Cmimi'];
+                            if ($totaliCmimit === 0) {
+                                $transporti = 0;
+                            } else {
+                                $transporti = 5;
                             }
-                            else{
-                                $transporti=5; 
-                            }
-                           
+
                             echo "
                         <tr>
                            <td>$order[piktura_id]</td>
@@ -61,8 +78,8 @@
                            <td><a class='btn btn-danger btn-sm' href='delete2.php?id=$order[ID]'>Fshije</a></td>
                         <tr> ";
                         }
-                        $totali=$totaliCmimit+$transporti;
-                        echo"
+                        $totali = $totaliCmimit + $transporti;
+                        echo "
                         <thead class='table-secondary'>
     <tr>
         <th>Cmimi</th>
@@ -74,17 +91,18 @@
     <td>$transporti</td>
     <td>$totali</td>
 </tr> 
-</thead>";}
+</thead>";
+                    }
 
-                 ?>
+                    ?>
 
                 </tbody>
-                    </table>
+            </table>
 
         </div>
-        <a style='margin-left: 850px;' class='btn btn-danger btn-sm' href='delete2.php'>Order</a></td>
+        <a style='margin-left: 850px; ' class='buttoni' href='delete2.php'>Order</a>
     </div>
-   
+
 </body>
 
 <?php include "Footer.php";  ?>
